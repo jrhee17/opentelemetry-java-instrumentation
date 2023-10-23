84f6f4384a2e46486fb6f2e4342403b0b9959fb3
jrhee17 <guins_j@guins.org>
2023-10-23 11:49:26 +0900

    ensure instrumentation is done max once per client

--- instrumentation/armeria-1.3/library/src/main/java/io/opentelemetry/instrumentation/armeria/v1_3/OpenTelemetryClient.java
+++ instrumentation/armeria-1.3/library/src/main/java/io/opentelemetry/instrumentation/armeria/v1_3/OpenTelemetryClient.java
@@ -11,12 +11,16 @@ import com.linecorp.armeria.client.SimpleDecoratingHttpClient;
 import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.logging.RequestLog;
+import io.netty.util.AttributeKey;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 
 /** Decorates an {@link HttpClient} to trace outbound {@link HttpResponse}s. */
 final class OpenTelemetryClient extends SimpleDecoratingHttpClient {
+
+  private static final AttributeKey<Boolean> OTEL_DECORATOR_SET =
+      AttributeKey.valueOf(OpenTelemetryClient.class, "OTEL_DECORATOR_SET");
 
   private final Instrumenter<ClientRequestContext, RequestLog> instrumenter;
 
@@ -28,6 +32,13 @@ final class OpenTelemetryClient extends SimpleDecoratingHttpClient {
 
   @Override
   public HttpResponse execute(ClientRequestContext ctx, HttpRequest req) throws Exception {
+    // just in case the decorator is set in the chain more than once
+    final Boolean isOtelDecoratorSet = ctx.attr(OTEL_DECORATOR_SET);
+    if (Boolean.TRUE.equals(isOtelDecoratorSet)) {
+      return unwrap().execute(ctx, req);
+    }
+    ctx.setAttr(OTEL_DECORATOR_SET, true);
+
     Context parentContext = Context.current();
     if (!instrumenter.shouldStart(parentContext, ctx)) {
       return unwrap().execute(ctx, req);
